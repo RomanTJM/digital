302c67f85706ddc8503cf8f1526a6f9ba8c5e3d1
Пользователь <ryabchenkora72@gmail.com>
2025-05-16 20:25:02 +0400

    13

--- backend/server.js
+++ backend/server.js
@@ -64,6 +64,10 @@ app.get('/selection', (req, res) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 }); 
